5431d78200538c2a7fd31f545ccd2cb95920a60c
Matyas <rusnak.matyas07@gmail.com>
2026-06-21 15:26:37 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Navigator from './Components/Navigator'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <Navigator></Navigator>
+
+
+    </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navigator.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Navigator() {
+    return (
+        <>
+            <div className="text-red-500 p-4">
+                Navigator OK (Tailwind)
+            </div>
+        </>
+    )
+}
+
