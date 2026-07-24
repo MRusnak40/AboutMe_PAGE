799a9229452a4ac8631b9a900a7727b492921dd8
Matyas <rusnak.matyas07@gmail.com>
2026-07-24 21:06:28 +0200

    fix(config files):set Config files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/AboutMe_PAGE/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
